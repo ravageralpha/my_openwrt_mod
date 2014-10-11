6a21b9f93bb30702f60e8639f410977eafb19428
ravageralpha <ravageralpha@gmail.com>
2014-10-11 15:21:36 +0800

    LuCI: Added protocol support for ShadowVPN and some missing translate file

---
+++ luci/protocols/shadowvpn/luasrc/model/cbi/admin_network/proto_shadowvpn.lua
@@ -0,0 +1,58 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2011 RA <ravageralpha@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+]]--
+
+local map, section = ...
+local fs = require "nixio.fs"
+
+local server, port, password
+
+server = section:taboption("general", Value, "server", translate("VPN Server"))
+server.datatype = "host"
+server.optional = false
+
+port = section:taboption("general", Value, "port", translate("Port"))
+port.datatype = "port"
+port.optional = false
+
+password = section:taboption("general", Value, "password", translate("Password"))
+password.password = true
+password.optional = false
+
+clientup = section:taboption("general", TextValue, "clientup", "client_up.sh")
+clientup.template = "cbi/tvalue"
+clientup.rows = 15
+
+function clientup.cfgvalue(self, section)
+	return fs.readfile("/etc/shadowvpn/client_up.sh") or ""
+end
+
+function clientup.write(self, section, value)
+	if value then
+		value = value:gsub("\r\n?", "\n")
+		fs.writefile("/etc/shadowvpn/client_up.sh", value)
+	end
+end
+
+clientdown = section:taboption("general", TextValue, "clientdown", "client_down.sh")
+clientdown.template = "cbi/tvalue"
+clientdown.rows = 15
+
+function clientdown.cfgvalue(self, section)
+	return fs.readfile("/etc/shadowvpn/client_down.sh") or ""
+end
+
+function clientdown.write(self, section, value)
+	if value then
+		value = value:gsub("\r\n?", "\n")
+		fs.writefile("/etc/shadowvpn/client_down.sh", value)
+	end
+end
---
+++ luci/protocols/shadowvpn/root/lib/netifd/shadowvpn.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+. /lib/functions.sh
+. ../netifd-proto.sh
+init_proto "$@"
+
+proto_shadowvpn_init_config() {
+	proto_config_add_string "server"
+	proto_config_add_int "port"
+	proto_config_add_string "password"
+	no_device=1
+	available=1
+}
+
+proto_shadowvpn_setup() {
+	local config="$1"
+
+	json_get_vars server port password
+
+	grep -q tun /proc/modules || insmod tun
+
+	logger -t shadowvpn "initializing..."
+
+	[ -z "$server" ] || [ -z "$port" ] || [ -z "$password" ] && {
+		logger -t shadowvpn "Missing required configuration"
+		proto_setup_failed "$config"
+		exit 1
+	}
+
+	mkdir -p /var/etc
+	sed -e "s#|SERVER|#$server#g" \
+		-e "s#|PORT|#$port#g" \
+		-e "s#|PASSWORD|#$password#g" \
+		/etc/shadowvpn/client.conf.template > /var/etc/shadowvpnclient.conf
+
+	proto_export INTERFACE="$config"
+	logger -t shadowvpn "executing ShadowVPN"
+	proto_run_command "$config" \
+	/usr/bin/shadowvpn -c /var/etc/shadowvpnclient.conf
+}
+
+proto_shadowvpn_teardown() {
+	local config="$1"
+	logger -t shadowvpn "bringing down ShadowVPN"
+	proto_kill_command "$config"
+	rm /var/etc/shadowvpnclient.conf
+}
+
+add_protocol shadowvpn
