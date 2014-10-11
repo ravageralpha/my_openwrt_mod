6b951f89a9698bceb384f5493864451347b78c58
ravageralpha <ravageralpha@gmail.com>
2014-10-11 15:30:43 +0800

    Fixing odd

--- luci/protocols/shadowvpn/luasrc/model/cbi/admin_network/proto_shadowvpn.lua
+++ luci/protocols/shadowvpn/luasrc/model/cbi/admin_network/proto_shadowvpn.lua
@@ -10,7 +10,7 @@ You may obtain a copy of the License at
 	http://www.apache.org/licenses/LICENSE-2.0
 ]]--
 
-local map, section = ...
+local map, section, net = ...
 local fs = require "nixio.fs"
 
 local server, port, password
