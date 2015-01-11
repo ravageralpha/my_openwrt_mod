a6c88d65858d0d135002e85bd4710ab8b905fc62
ravageralpha <ravageralpha@gmail.com>
2015-01-11 11:22:34 +0800

    LuCI: some tweak for luci-chinadns

--- luci/applications/luci-chinadns/luasrc/model/cbi/chinadns.lua
+++ luci/applications/luci-chinadns/luasrc/model/cbi/chinadns.lua
@@ -50,10 +50,15 @@ chn.wrap = "off"
 function chn.cfgvalue(self, section)
 	return fs.readfile("/etc/chinadns_chnroute.txt") or ""
 end
+
 function chn.write(self, section, value)
 	if value then
 		value = value:gsub("\r\n?", "\n")
-		fs.writefile("/etc/chinadns_chnroute.txt", value)
+		fs.writefile("/tmp/chinadns_chnroute.txt", value)
+		if (fs.access("/etc/chinadns_chnroute.txt") ~= true or luci.sys.call("cmp -s /tmp/chinadns_chnroute.txt /etc/chinadns_chnroute.txt") == 1) then
+			fs.writefile("/etc/chinadns_chnroute.txt", value)
+		end
+		fs.remove("/tmp/chinadns_chnroute.txt")
 	end
 end
 
