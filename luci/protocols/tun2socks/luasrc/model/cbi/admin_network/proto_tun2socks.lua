1c8d8c2c173152762c8bf00f999aaf2a379e83c7
ravageralpha <ravageralpha@gmail.com>
2016-01-13 15:19:45 +0800

    add luci-proto-tun2socks

---
+++ luci/protocols/tun2socks/luasrc/model/cbi/admin_network/proto_tun2socks.lua
@@ -0,0 +1,68 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2011 RA <ravageralpha@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+]]--
+
+local map, section, net = ...
+local fs = require "nixio.fs"
+
+local server, remote, ifname, localnet, udprelay, opts
+
+server = section:taboption("general", Value, "server", translate("Socks Server"))
+server.placeholder = "127.0.0.1:1080"
+server.optional = false
+
+remote = section:taboption("general", Value, "remote", translate("Actual Server IP Address"))
+remote.datatype = ipaddr
+remote.optional = false
+
+udprelay = section:taboption("general", Flag, "udprelay", translate("Enable UDP Relay"))
+
+localnet = section:taboption("general", Value, "localnet", translate("Local Address"))
+localnet.default = "10.0.0.2/24"
+localnet.placeholder = "10.0.0.2/24"
+localnet.optional = false
+
+opts = section:taboption("general", Value, "opts", translate("Extra Options"))
+opts.rmempty = true
+opts.optional = true
+
+ifname = section:taboption("general", Value, "interface", translate("Output Interface"))
+ifname.template = "cbi/network_netlist"
+
+clientup = section:taboption("advanced", TextValue, "clientup", "client_up.sh")
+clientup.template = "cbi/tvalue"
+clientup.rows = 15
+
+function clientup.cfgvalue(self, section)
+	return fs.readfile("/etc/tun2socks/client_up.sh") or ""
+end
+
+function clientup.write(self, section, value)
+	if value then
+		value = value:gsub("\r\n?", "\n")
+		fs.writefile("/etc/tun2socks/client_up.sh", value)
+	end
+end
+
+clientdown = section:taboption("advanced", TextValue, "clientdown", "client_down.sh")
+clientdown.template = "cbi/tvalue"
+clientdown.rows = 15
+
+function clientdown.cfgvalue(self, section)
+	return fs.readfile("/etc/tun2socks/client_down.sh") or ""
+end
+
+function clientdown.write(self, section, value)
+	if value then
+		value = value:gsub("\r\n?", "\n")
+		fs.writefile("/etc/tun2socks/client_down.sh", value)
+	end
+end
---
+++ luci/protocols/tun2socks/root/lib/netifd/proto/tun2socks.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+. /lib/functions.sh
+. ../netifd-proto.sh
+init_proto "$@"
+
+proto_tun2socks_init_config() {
+	proto_config_add_string "server"
+	proto_config_add_string "remote"
+	proto_config_add_string "interface"
+	proto_config_add_string "localnet"
+	proto_config_add_int "udprelay"
+	proto_config_add_string "opts"
+	no_device=1
+	available=1
+}
+
+proto_tun2socks_setup() {
+	local config="$1"
+
+	json_get_vars server interface localnet remote udprelay opts
+
+	grep -q tun /proc/modules || insmod tun
+
+	logger -t tun2socks "initializing..."
+	proto_add_host_dependency "$config" "$remote" $interface
+	proto_export INTERFACE="$config"
+	logger -t tun2socks "executing tun2socks"
+
+	local netmask="`/bin/ipcalc.sh $localnet | grep NETMASK | cut -d'=' -f2`"
+
+	proto_run_command "$config" \
+	/usr/bin/tun2socks \
+		--tundev "vpn-$config" \
+		--netif-ipaddr "$localnet" \
+		--netif-netmask "$netmask" \
+		--socks-server-addr "$server" \
+		${udprelay:+--enable-udprelay} \
+		${opts:+$opts}
+
+	[ -f "/etc/tun2socks/client_up.sh" ] && {
+		/bin/sh /etc/tun2socks/client_up.sh \
+			"$config" "vpn-$config" "$localnet"
+	}
+}
+
+proto_tun2socks_teardown() {
+	local config="$1"
+	logger -t tun2socks "bringing down tun2socks"
+	proto_kill_command "$config"
+	[ -f "/etc/tun2socks/client_down.sh" ] && {
+		/bin/sh /etc/tun2socks/client_down.sh \
+			"$config" "vpn-$config"
+	}
+}
+
+add_protocol tun2socks
